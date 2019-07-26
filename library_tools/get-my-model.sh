14b7afd81aabd6574112e67be67e81a5bcd85a93
Joao Cerqueira <jpacerqueira@gmail.com>
2019-07-26 16:30:18 +0100

    Improved version --31min time limited on testing

--- library_tools/get-my-model.sh
+++ library_tools/get-my-model.sh
@@ -3,6 +3,6 @@ rm -rf  $HOME/tmpdir
 mkdir $HOME/tmpdir
 cd $HOME/tmpdir
 curl http://127.0.0.1:54321/3/h2o-genmodel.jar > h2o-genmodel.jar
-curl http://127.0.0.1:54321/3/Models.java/DeepLearning_grid_1_AutoML_20190726_110849_model_3 > DeepLearning_grid_1_AutoML_20190726_110849_model_3.java
-echo ' (RUN AS):~$  javac -cp h2o-genmodel.jar -J-Xmx2g -J-XX:MaxPermSize=128m DeepLearning_grid_1_AutoML_20190726_110849_model_3.java '
+curl http://127.0.0.1:54321/3/Models.java/GBM_grid_1_AutoML_20190726_145926_model_5 > GBM_grid_1_AutoML_20190726_145926_model_5.java
+echo ' (RUN AS):~$  javac -cp h2o-genmodel.jar -J-Xmx2g -J-XX:MaxPermSize=128m GBM_grid_1_AutoML_20190726_145926_model_5.java '
 #
