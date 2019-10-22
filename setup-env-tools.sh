3ac9d05234314e20f486db6a56cfe6750a73d365
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2019-10-22 13:12:23 +0100

    Update setup-env-tools.sh

--- setup-env-tools.sh
+++ setup-env-tools.sh
@@ -2,7 +2,9 @@
 #
 # git clone https://github.com/jpacerqueira/Jupyter_Spark_H2O_Kafka_Client_Setup.git
 #
-#cd . ; cp library_tools/*.sh . ; bash -x anaconda_setup.sh
+#cd  ; cp library_tools/*.sh . ; bash -x anaconda_setup.sh
+#
+cd $HOME
 #
 bash -x anaconda_setup.sh 
 #
