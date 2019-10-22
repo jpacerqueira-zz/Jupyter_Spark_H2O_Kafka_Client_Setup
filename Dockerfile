d9d71ea3bf431f6fe24ae49d0f666835bf429771
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2019-10-22 12:54:13 +0100

    Updated_Execution
    Updated Execution

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ ADD setup-env-tools.sh /usr/local/bin/setup-env-tools.sh
 
 RUN chmod 777 /usr/local/bin/setup-env-tools.sh
 
+RUN cp -rf library_tools/*.sh $HOME ;
+
 CMD /usr/local/bin/setup-env-tools.sh ; sleep infinity
--- setup-env-tools.sh
+++ setup-env-tools.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-git clone https://github.com/jpacerqueira/Jupyter_Spark_H2O_Kafka_Client_Setup.git
+# git clone https://github.com/jpacerqueira/Jupyter_Spark_H2O_Kafka_Client_Setup.git
 #
 #cd . ; cp library_tools/*.sh . ; bash -x anaconda_setup.sh
 #
-cp -rf Jupyter_Spark_H2O_Kafka_Client_Setup/library_tools/*.sh ; bash -x anaconda_setup.sh 
+bash -x anaconda_setup.sh 
 #
 sleep 1
 service supervisor start
