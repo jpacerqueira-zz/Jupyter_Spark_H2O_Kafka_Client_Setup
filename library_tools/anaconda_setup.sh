ce7395509bbddeb3f234614fe31287eed5f8e9c5
jpacerqueira <jpacerqueira@gmail.com>
2019-10-25 16:59:31 +0100

    Dockerfile idempotency re-established v1.2

--- library_tools/anaconda_setup.sh
+++ library_tools/anaconda_setup.sh
@@ -5,7 +5,7 @@ cd anaconda3
 curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 sha256sum Anaconda3-2019.03-Linux-x86_64.sh
 sudo chmod +x Anaconda3-2019.03-Linux-x86_64.sh
-yes | ./Anaconda3-2019.03-Linux-x86_64.sh
+yes | bash -x Anaconda3-2019.03-Linux-x86_64.sh
 cd ..
 conda activate base
 bash install_packages.sh
