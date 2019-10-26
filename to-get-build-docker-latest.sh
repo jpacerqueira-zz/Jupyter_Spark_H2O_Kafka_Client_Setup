3b7fc67e1a0c15a8452bc5f95328798ffb25c8dc
jpacerqueira <jpacerqueira@gmail.com>
2019-10-26 07:32:27 +0100

    Dockerfile idempotency re-established v2.0 - recorded docker-commands

--- to-get-build-docker-latest.sh
+++ to-get-build-docker-latest.sh
@@ -7,4 +7,12 @@ docker run $IMG_LATEST
 #
 ###### MOUNT VOLUME ### docker run -it -v ~/Downloads:/Downloads $IMG_LATEST bash
 #
-## docker exec -it $IMG_LATEST bash /bin/bash 
+## docker exec -it  $(docker ps -a | grep $IMG_LATEST |  awk -F' ' '{ print $1 }') /bin/bash 
+#
+##
+#### export MY_RUNNING_CONTAINER=$(docker ps -a | grep $IMG_LATEST |  awk -F' ' '{ print $1 }' )
+## 
+### docker stop MY_RUNNING_CONTAINER
+##
+### MOUNT VOLUME LATEST RUN ### docker run -it -v ~/Downloads:/Downloads $MY_RUNNING_CONTAINER bash
+##  
