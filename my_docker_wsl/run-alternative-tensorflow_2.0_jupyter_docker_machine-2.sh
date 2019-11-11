d6af640771fb930a9016475b396d58bb4a22a00e
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2019-11-11 17:51:04 +0000

    Add files via upload

--- my_docker_wsl/run-alternative-tensorflow_2.0_jupyter_docker_machine-2.sh
+++ my_docker_wsl/run-alternative-tensorflow_2.0_jupyter_docker_machine-2.sh
@@ -6,9 +6,10 @@ docker pull tensorflow/tensorflow:nightly-py3-jupyter
 IMAGE_TSFLOW=$(docker image ls | grep tensorflow | awk -F' ' '{ print $1 }')
 echo ${IMAGE_TSFLOW}
 #:-WSL-:#
-FILE1=Setup-System-Java-Spark-h2o.ai-pyarrow.py# #py or ipynb in test to load additional libraries
+FILE1=Setup-System-Java-Spark-h2o.ai-pyarrow.ipynb ##py or ipynb in test to load additional libraries
 if [ -f "/mnt/c/Users/joci/Documents/GitHub/Jupyter_Spark_H2O_Kafka_Client_Setup/notebooks/$FILE1" ]; then
-    docker run -it -p 9003:8888 -v c:/Users/joci/Documents/GitHub/Jupyter_Spark_H2O_Kafka_Client_Setup/notebooks:/tf/notebooks_h2o ${IMAGE_TSFLOW}:nightly-py3-jupyter python /tf/notebooks_h2o/$FILE1
+    docker run -it -p 9003:8888 -v c:/Users/joci/Documents/GitHub/Jupyter_Spark_H2O_Kafka_Client_Setup/notebooks:/tf/notebooks_h2o ${IMAGE_TSFLOW}:nightly-py3-jupyter jupyter nbconvert --to notebook --execute --allow-errors --ExecutePreprocessor.timeout=180 /tf/notebooks_h2o/$FILE1 
+#   docker run -it -p 9003:8888 -v c:/Users/joci/Documents/GitHub/Jupyter_Spark_H2O_Kafka_Client_Setup/notebooks:/tf/notebooks_h2o ${IMAGE_TSFLOW}:nightly-py3-jupyter python /tf/notebooks_h2o/$FILE1 
 else 
     docker run -it -p 9003:8888 -v c:/Users/joci/Documents/GitHub/Jupyter_Spark_H2O_Kafka_Client_Setup/notebooks:/tf/notebooks_h2o ${IMAGE_TSFLOW}:nightly-py3-jupyter
 fi
