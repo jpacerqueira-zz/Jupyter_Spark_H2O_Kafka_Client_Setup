8064489dabeb946b91ecc2cf66853873fd154a3d
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2019-11-11 15:18:32 +0000

    Add files via upload

--- my_docker_wsl/run-alternative-tensorflow_2.0_jupyter_docker_machine-2.sh
+++ my_docker_wsl/run-alternative-tensorflow_2.0_jupyter_docker_machine-2.sh
@@ -6,7 +6,7 @@ docker pull tensorflow/tensorflow:nightly-py3-jupyter
 IMAGE_TSFLOW=$(docker image ls | grep tensorflow | awk -F' ' '{ print $1 }')
 echo ${IMAGE_TSFLOW}
 #:-WSL-:#
-FILE1=Setup-System-Java-Spark-h2o.ai-pyarrow.py
+FILE1=Setup-System-Java-Spark-h2o.ai-pyarrow.ipynb
 if [ -f "/mnt/c/Users/joci/Documents/GitHub/Jupyter_Spark_H2O_Kafka_Client_Setup/notebooks/$FILE1" ]; then
     docker run -it -p 9003:8888 -v c:/Users/joci/Documents/GitHub/Jupyter_Spark_H2O_Kafka_Client_Setup/notebooks:/tf/notebooks_h2o ${IMAGE_TSFLOW}:nightly-py3-jupyter python /tf/notebooks_h2o/$FILE1
 else 
