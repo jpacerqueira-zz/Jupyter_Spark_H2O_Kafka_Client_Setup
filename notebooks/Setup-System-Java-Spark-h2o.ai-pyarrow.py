28d0d3b15f300003f0f679e8d66c5cdfedf1f106
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2019-11-11 13:55:35 +0000

    Add files via upload

--- notebooks/Setup-System-Java-Spark-h2o.ai-pyarrow.py
+++ notebooks/Setup-System-Java-Spark-h2o.ai-pyarrow.py
@@ -4,67 +4,70 @@
 # In[1]:
 
 
-!apt-get install wget
-!apt-get install tar
-!apt-get install openjdk-8-jdk-headless -qq > /dev/null
-!wget -q https://archive.apache.org/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
-!tar xf spark-2.4.4-bin-hadoop2.7.tgz
+get_ipython().system('apt-get install wget')
+get_ipython().system('apt-get install tar')
+get_ipython().system('apt-get install openjdk-8-jdk-headless -qq > /dev/null')
+get_ipython().system('wget -q https://archive.apache.org/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz')
+get_ipython().system('tar xf spark-2.4.4-bin-hadoop2.7.tgz')
 
 
 # In[2]:
 
 
-!pip install h2o
+get_ipython().system('pip install h2o')
 
 
 # In[3]:
 
 
-!pip install findspark
+get_ipython().system('pip install findspark')
 
 
 # In[4]:
 
 
-!pip install pyarrow
+get_ipython().system('pip install pyarrow')
 
 
 # In[5]:
 
 
-!pip install pandas
+get_ipython().system('pip install pandas')
 
 
 # In[6]:
 
 
-!pip install numpy
+get_ipython().system('pip install numpy')
 
 
 # In[7]:
 
 
-!cat $HOME/.bashrc >> log.txt
+import h2o
+h2o.init()
+
 
 # In[8]:
 
 
 import subprocess 
-!echo 'PATH=$HOME/spark-2.4.4-bin-hadoop2.7/bin:\$PATH' >> $HOME/.bashrc 
-!echo 'JAVA_HOME=/usr/lib/jvm/openjdk1.8.0_222' >> $HOME./bashrc 
-!echo 'SPARK_HOME=$HOME/spark/spark-2.4.4-bin-hadoop2.7' >> $HOME./bashrc
-!echo 'HADOOP_HOME=$SPARK_HOME' >> $HOME/.bashrc
-!echo 'JAVA_HOME=/usr/lib/jvm/jdk1.8.0_222' >> $HOME/.bashrc
-!source $HOME/.bashrc
+cmd0 = "cat $HOME/.bashrc "
+returned_value0 = subprocess.call(cmd0, shell=True) 
+print("returned value:", returned_value0)
 
 
 # In[9]:
 
-import h2o
-h2o.init()
+
+import subprocess 
+cmd1 = "echo 'PATH=\$HOME/spark-2.4.4-bin-hadoop2.7/bin:\$PATH' >> \$HOME/.bashrc ;  echo 'JAVA_HOME=/usr/lib/jvm/openjdk1.8.0_222' >> \$HOME./bashrc ;  echo 'SPARK_HOME=\$HOME/spark/spark-2.4.4-bin-hadoop2.7' >> \$HOME./bashrc ;  echo 'HADOOP_HOME=\$SPARK_HOME' >> \$HOME/.bashrc ;  echo 'JAVA_HOME=/usr/lib/jvm/jdk1.8.0_222' >> \$HOME/.bashrc ;  source \$HOME/.bashrc " 
+returned_value1 = subprocess.call(cmd1, shell=True) 
+print("returned value:", returned_value1)
 
 
-# In[ 10]:
+# In[10]:
 
 
 exit()
+
