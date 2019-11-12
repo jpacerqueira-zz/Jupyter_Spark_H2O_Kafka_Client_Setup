aecde9458d93c98005a7c0a5976a6b95fa746e9d
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2019-11-12 17:37:15 +0000

    Update install-start-citus-cluster.sh

--- postgresql_citus/install-start-citus-cluster.sh
+++ postgresql_citus/install-start-citus-cluster.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+#
+docker-machine start default
 curl -L https://raw.githubusercontent.com/citusdata/docker/master/docker-compose.yml > docker-compose.yml
 COMPOSE_PROJECT_NAME=citus docker-compose up -d
+#
