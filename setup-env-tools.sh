0bf5dc01449caf1f2085d511a1dff211daf14b57
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2020-02-10 16:40:54 +0000

    Update setup-env-tools.sh
    Change back for a wait of 180 seconds to anaconda_setup.sh

--- setup-env-tools.sh
+++ setup-env-tools.sh
@@ -22,8 +22,8 @@ echo 'export JAVA_HOME=/usr/lib/jvm/default-java' >> $HOME/.bashrc
 #
 nohup bash -x anaconda_setup.sh > $HOME/knode_ds.out 2> $HOME/knode_ds.err
 #
-echo 'Sleep for 5 secs'
-sleep 5
+echo 'Sleep for 3 minutes'
+sleep 181
 ##
 echo 'knode_ds.out'
 cat $HOME/knode_ds.out
