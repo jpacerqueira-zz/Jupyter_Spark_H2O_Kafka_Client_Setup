65bd6154f200421c0dc5a2b5620e05fbeb42a89f
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2020-02-13 22:35:30 +0000

    Update install_packages.sh
    spark 2.4.5 typo fixed

--- library_tools/install_packages.sh
+++ library_tools/install_packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 bash setup_install_java.sh
-bash setup_install_spark.2.4.4.sh
+bash setup_install_spark.2.4.5.sh
 bash setup_install_findspark.sh
 bash setup_install_h2o.sh
 bash setup_install_kafka.sh
