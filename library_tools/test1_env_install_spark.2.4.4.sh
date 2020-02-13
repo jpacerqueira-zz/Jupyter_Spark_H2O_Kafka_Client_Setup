b5ef26ffd9f4946001492cfc04451c55cc400867
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2020-02-13 09:31:54 +0000

    Update test1_env_install_spark.2.4.4.sh

--- library_tools/test1_env_install_spark.2.4.4.sh
+++ library_tools/test1_env_install_spark.2.4.4.sh
@@ -8,7 +8,7 @@ mv spark-2.4.4-bin-hadoop2.7.tgz $HOME/spark
 cd $HOME/spark
 tar -zvxf spark-2.4.4-bin-hadoop2.7.tgz
 # env
-echo  'export PATH="home/joci/spark/spark-2.4.4-bin-hadoop2.7/bin:$PATH"' >> $HOME/.bashrc
+echo  'export PATH="$HOME/spark/spark-2.4.4-bin-hadoop2.7/bin:$PATH"' >> $HOME/.bashrc
 source $HOME/.bashrc
 #
 # Force pyspark for Jupyter
