21d85e6c4a1b84913b4c940eaaf3796e1bd91fa6
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2020-02-14 00:47:47 +0000

    Update install_packages.sh

--- library_tools/install_packages.sh
+++ library_tools/install_packages.sh
@@ -4,5 +4,7 @@ bash setup_install_spark.2.4.5.sh
 bash setup_install_findspark.sh
 bash setup_install_h2o.sh
 bash setup_install_kafka.sh
-bash setup_jupyter_start_notebooks.sh
+bash install-pyarrow.sh
+#
+#bash setup_jupyter_start_notebooks.sh
 #
