37596a09506d8f307ca6b5505962bb7d311434c6
jpacerqueira <jpacerqueira@gmail.com>
2020-02-26 19:11:34 +0000

    Control run-alternative with RUN_INSTALL status

--- my_docker_wsl/run-alternative-tensorflow_2.0_jupyter_docker_machine-2.sh
+++ my_docker_wsl/run-alternative-tensorflow_2.0_jupyter_docker_machine-2.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #
-#!/bin/bash
+# RUN_INSTALL=1 installs python libraries via Notebook
 #
+RUN_INSTALL=$1
 docker pull tensorflow/tensorflow:nightly-py3-jupyter
 IMAGE_TSFLOW=$(docker image ls | grep tensorflow | awk -F' ' '{ print $1 }')
 echo ${IMAGE_TSFLOW}
+#
 #:-WSL-:# && #:-VBox-:#
 FILE1=Setup-System-Java-Spark-h2o.ai-pyarrow.ipynb # # ipynb in test to load additional libraries
 PWD1=$(cd .. ; pwd)
-if [ -f "${PWD1}/notebooks/${FILE1}" ]; then
+if [ "$RUN_INSTALL" == "1"  ]  && [ -f "${PWD1}/notebooks/${FILE1}" ]; then
     docker run -it -p 9003:8888 -v ${PWD1}/notebooks:/tf/notebooks_h2o ${IMAGE_TSFLOW}:nightly-py3-jupyter jupyter nbconvert --to notebook --execute --allow-errors --ExecutePreprocessor.timeout=180 /tf/notebooks_h2o/$FILE1 
 else 
     docker run -it -p 9003:8888 -v ${PWD1}/notebooks:/tf/notebooks_h2o ${IMAGE_TSFLOW}:nightly-py3-jupyter
