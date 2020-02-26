eb0356b55949b321cb869b0902e37259493a692e
jpacerqueira <jpacerqueira@gmail.com>
2020-02-26 19:06:44 +0000

    small change big diff

--- my_docker_wsl/run-alternative-tensorflow_2.0_jupyter_docker_machine-2.sh
+++ my_docker_wsl/run-alternative-tensorflow_2.0_jupyter_docker_machine-2.sh
@@ -7,7 +7,7 @@ IMAGE_TSFLOW=$(docker image ls | grep tensorflow | awk -F' ' '{ print $1 }')
 echo ${IMAGE_TSFLOW}
 #:-WSL-:# && #:-VBox-:#
 FILE1=Setup-System-Java-Spark-h2o.ai-pyarrow.ipynb # # ipynb in test to load additional libraries
-PWD1=$(cd .. | pwd)
+PWD1=$(cd .. ; pwd)
 if [ -f "${PWD1}/notebooks/${FILE1}" ]; then
     docker run -it -p 9003:8888 -v ${PWD1}/notebooks:/tf/notebooks_h2o ${IMAGE_TSFLOW}:nightly-py3-jupyter jupyter nbconvert --to notebook --execute --allow-errors --ExecutePreprocessor.timeout=180 /tf/notebooks_h2o/$FILE1 
 else 
