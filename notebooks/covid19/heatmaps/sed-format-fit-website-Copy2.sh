dc4b33f5eede9e6460497713ee33d87e5686ab4e
jpacerqueira <jpacerqueira@gmail.com>
2021-04-06 18:45:50 +0100

    New - +daily spark reconciliation analysis  - 06Apr2021 - #COVID19

---
+++ notebooks/covid19/heatmaps/sed-format-fit-website-Copy2.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+FILEAN=$1
+#
+## operates outside docker in shared folder
+#
+#HOMEY=/home/notebookuser
+#source $HOMEY/.profile
+#cd $HOMEY/notebooks/covid19/heatmaps
+#
+sed -i -e 's/\n //g' $FILEAN
+sed -i -e 's/\n //g' $FILEAN
+sed -i -e 's/\n //g' $FILEAN
+sed -i -e 's/\n //g' $FILEAN
+#
+sed -i -e 's/   / /g' $FILEAN
+sed -i -e 's/   / /g' $FILEAN
+sed -i -e 's/   / /g' $FILEAN
+sed -i -e 's/   / /g' $FILEAN
+#
+sed -i -e 's/  / /g' $FILEAN
+sed -i -e 's/  / /g' $FILEAN
+sed -i -e 's/  / /g' $FILEAN
+#
+#
